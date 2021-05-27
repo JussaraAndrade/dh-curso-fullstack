55d42ab965966e58df839f3da1d2c9d23bd65a8c
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-05-27 20:54:27 -0300

    fix: testes com karma angular

--- Angular/angular crud/crud-angular/src/app/app.component.spec.ts
+++ Angular/angular crud/crud-angular/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,27 +11,36 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
       ],
     }).compileComponents();
   });
-
+  // Primeiroa teste
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // Espera o app ele seja true
     expect(app).toBeTruthy();
   });
 
+  // Segundo teste
   it(`should have as title 'crud-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('crud-angular');
   });
 
+  it('should return isOpen is false', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.isOpen).toBe(false);
+  })
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('crud-angular app is running!');
+    expect(compiled.querySelector('.content').textContent).toContain('crud-angular app is running!');
   });
 });
--- Angular/angular crud/crud-angular/src/app/services/users.service.spec.ts
+++ Angular/angular crud/crud-angular/src/app/services/users.service.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
@@ -5,10 +7,36 @@ import { UsersService } from './users.service';
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockResponse = [{
+    id: 1,
+    nome: 'Juca',
+    sobrenome: 'Andrade',
+    idade: 26,
+    profissao: "Programador"
+  }];
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+
+      ]
+    });
     service = TestBed.inject(UsersService);
   });
+
+  it('should be return response users', () => {
+    service.getUsers().subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    })
+  })
+
+  it('should be return user response', () => {
+    service.getUser('Juca').subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    })
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
